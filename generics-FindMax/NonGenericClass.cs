cb4d880eeaeca81914fed69417b36338672c56f6
Nikhil Kumar Yadav <nikhilyadavindigo@gmail.com>
2020-10-24 09:11:09 +0530

    [nikhil]FloatMax

--- generics-FindMax/NonGenericClass.cs
+++ generics-FindMax/NonGenericClass.cs
@@ -2,9 +2,58 @@
 using System.Collections.Generic;
 using System.Text;
 
-namespace generics_FindMax
+namespace FindMaxProblem
 {
-    class NonGenericClass
+    public class NonGenericClass
     {
+        /// <summary>
+        /// max of 3 integers using non generic method
+        /// </summary>
+        /// <param name="first"></param>
+        /// <param name="second"></param>
+        /// <param name="third"></param>
+        /// <returns></returns>
+        public static int MaxIntegerNumberAmongThree(int first, int second, int third)
+        {
+            Console.WriteLine("Maximum number");
+            //compare to method is used to compare 2 numbers
+            if (first.CompareTo(second) > 0 && first.CompareTo(third) > 0)
+            {
+                return first;
+            }
+            if (second.CompareTo(first) > 0 && second.CompareTo(third) > 0)
+            {
+                return second;
+            }
+            if (third.CompareTo(second) > 0 && third.CompareTo(first) > 0)
+            {
+                return third;
+            }
+            throw new Exception("All 3 numbers are same");
+        }
+        /// <summary>
+        /// used to find max between 3 floats using non generic method
+        /// </summary>
+        /// <param name="first"></param>
+        /// <param name="second"></param>
+        /// <param name="third"></param>
+        /// <returns></returns>
+        public static float MaxfloatNumberAmongThree(float first, float second, float third)
+        {
+            Console.WriteLine("Maximum number");
+            if (first.CompareTo(second) > 0 && first.CompareTo(third) > 0)
+            {
+                return first;
+            }
+            if (second.CompareTo(first) > 0 && second.CompareTo(third) > 0)
+            {
+                return second;
+            }
+            if (third.CompareTo(second) > 0 && third.CompareTo(first) > 0)
+            {
+                return third;
+            }
+            throw new Exception("All 3 numbers are same");
+        }
     }
 }
--- generics-FindMax/Program.cs
+++ generics-FindMax/Program.cs
@@ -1,12 +1,30 @@
 ﻿using System;
 
-namespace generics_FindMax
+namespace FindMaxProblem
 {
     class Program
     {
+        /// <summary>
+        /// Implementing different methods from different classes
+        /// </summary>
+        /// <param name="args"></param>
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("welcome to find max problem");
+            //used to catch exception
+            try
+            {
+                //Calling non generic method to find max of 3 integers
+                int maxInt = NonGenericClass.MaxIntegerNumberAmongThree(23, 24, 22);
+                Console.WriteLine("max integer value is\t" + maxInt);
+                //calling non generic method to find max of 3 floats
+                float maxFloat = NonGenericClass.MaxfloatNumberAmongThree(23, 24, 22);
+                Console.WriteLine("max float value is\t" + maxFloat);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
         }
     }
 }
